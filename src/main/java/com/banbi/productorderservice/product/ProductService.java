8fbb0a468e63d76cfbe511ab247e136e205ac38d
choibohyun <320fleur@gmail.com>
2024-05-15 16:58:39 +0900

    실전! 스프링부트 상품 주문 API 개발로 알아보는 TDD - Section01) API테스트로 전환하기 (완)

--- src/main/java/com/banbi/productorderservice/product/ProductService.java
+++ src/main/java/com/banbi/productorderservice/product/ProductService.java
@@ -1,9 +1,13 @@
 package com.banbi.productorderservice.product;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
-@Component
+@RestController
+@RequestMapping("/products")
 class ProductService {
 
     private final ProductPort productPort;
@@ -12,10 +16,12 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    @Transactional
-    public void addProduct(AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
